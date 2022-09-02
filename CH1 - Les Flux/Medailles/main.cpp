5d81b585ca02ad524d30824375d2a5a61a7d9dc4
mbrule <sogangpro@gmail.com>
2022-09-02 11:09:02 +0200

    Medailles/

--- CH1 - Les Flux/Medailles/main.cpp
+++ CH1 - Les Flux/Medailles/main.cpp
@@ -2,14 +2,14 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
-
+#include <fstream>
 using namespace std;
 int main() {
     string nomDuFichier;
     cout << "Entrer le nom du fichier à lire : ";
     cin >> nomDuFichier;
     //Création du flux en lecture sur le fichier
-    ifstream leFichier(nomDuFichier.c_str()); // c_str() transforme string en char*
+    ifstream leFichier(nomDuFichier.c_str()) ; // c_str() transforme string en char*
     if (!leFichier .is_open())
         cerr << "Erreur lors de l'ouverture du fichier" << endl;
     else
@@ -18,17 +18,25 @@ int main() {
         int nbOr;
         int nbArgent;
         int nbBronze;
-        cout << "+" << setfill('-') <<setw(18) << "+" <<setw(10) << "+" <<setw(10) << "+" <<setw(10) << "+" <<setfill(' ') <<endl ;
+
+        ofstream monFlux("../Medailles/resulat.txt");
+        if (!monFlux .is_open())
+            cerr << "Erreur lors de l'ouverture du fichier" << endl;
+            else {
+            monFlux << "+" << setfill('-') <<setw(18) << "+" <<setw(9) << "+" <<setw(9) << "+" <<setw(9) << "+" <<setfill(' ') <<endl ;
         do
         {
-            //récupération des valeurs
             leFichier >> pays >> nbOr >> nbArgent >> nbBronze;
             if (leFichier.good())//Si les valeurs ont bien été lues
             {
-                cout << "| " <<setw(17)  <<left <<pays  << "|" << setw(8) <<right <<nbOr << " |" << setw(8) <<right <<nbArgent << " |" << setw(8) <<right <<nbBronze << " |" << endl ;
+                monFlux << "|" <<setw(17)  <<left <<pays  << "|"
+                     << setw(7) <<right <<nbOr << " "<< "|"
+                     << setw(7) <<right <<nbArgent << " "<< "|"
+                     << setw(7) <<right <<nbBronze << " "<< "|" << endl ;
             }
         } while (!leFichier.eof());
-        cout << "+" << setfill('-') <<setw(19) << "+" <<setw(10) << "+" <<setw(10) << "+" <<setw(10) << "+" <<setfill(' ') <<endl ;
+        monFlux << "+" << setfill('-') <<setw(18) << "+" <<setw(9) << "+" <<setw(9) << "+" <<setw(9) << "+" <<setfill(' ') <<endl ;
+        }
     }
     return 0;
 }
