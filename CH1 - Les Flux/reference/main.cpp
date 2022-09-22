5a85d3773bf3f45ad7b2f2e7c867c60c35eda8aa
matysbrule <90618313+matysbrule@users.noreply.github.com>
2022-09-22 09:42:45 +0200

    references

---
+++ CH1 - Les Flux/reference/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a = 1;
+int b = -2;
+int c = 0;
+cout << "avant l'appel de Ajouter" << endl;
+cout << "a= " << a << endl;
+cout << "b= " << b << endl;
+cout << "c= " << c << endl;
+// Appel le la fonction Ajouter à compléter dans le tableau suivant le cas.
+cout << "après l'appel de Ajouter" << endl;
+cout << "a= " << a << endl;
+cout << "b= " << b << endl;
+cout << "c= " << c << endl;
+return 0;
+}
